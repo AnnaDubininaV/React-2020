350e266ecc494cbe939fd90c2460c6df2003fbbf
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-19 01:26:41 +0300

    Section-4: refactored category-container

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 const App = () => {
+  const categories = [
+    { id: 1, title: 'Hats' },
+    { id: 2, title: 'Jacketss' },
+    { id: 3, title: 'Sneakers' },
+    { id: 4, title: 'Womens' },
+    { id: 5, title: 'Mens' },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="caregory-container">
-        {/* < img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ title, id }) => (
+        <div key={id} className="caregory-container">
+          <div className="background-image"></div>
+          {/* < img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="caregory-container">
-        {/* < img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="caregory-container">
-        {/* < img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="caregory-container">
-        {/* < img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="caregory-container">
-        {/* < img /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
