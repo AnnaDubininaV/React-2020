666df7b3643f86aa23e1dc5cff66d2d6f4a33cc9
Anna Dubinina <anna.dybinina.1@gmail.com>
2022-08-21 00:09:24 +0300

    Section-14: Added CategoriesReducer and removed CategoriesContext

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
 
@@ -16,13 +14,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <UserProvider> */}
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-        {/* </UserProvider> */}
+        <CartProvider>
+          <App />
+        </CartProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { userReduser } from './user/user.reducer';
+import { categoriesReduser } from './categories/category.reducer';
 
 export const rootReducer = combineReducers({
   user: userReduser,
+  categories: categoriesReduser,
 });
